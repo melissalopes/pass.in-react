9073516ce86fa7da4cb2c27b3441d47427fca96a
Melissa Lopes Gouveia <k2.melissa@grupofleury.com.br>
2024-04-06 13:00:57 -0300

    feat: create nav link

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,13 +1,20 @@
-import NlwUniteIcon from "../assets/nlw-unite-icon.svg";
+import nlwUniteIcon from "../assets/nlw-unite-icon.svg";
+import { NavLink } from "./nav-link";
 
 export function Header() {
   return (
     <div className="flex items-center gap-5 py-2">
-      <img src={NlwUniteIcon}></img>
+      <img src={nlwUniteIcon} alt="NLW Unite" />
 
       <nav className="flex items-center gap-5">
-        <a href="" className="font-medium text-sm text-zinc-300">Eventos</a>
-        <a href="" className="font-medium text-sm">Participantes</a>
+        <a href="" className="font-medium text-sm text-zinc-300">
+          Eventos
+        </a>
+        <a href="" className="font-medium text-sm">
+          Participantes
+        </a>
+        <NavLink href="/eventos">Eventos</NavLink>
+        <NavLink href="/participantes">Participantes</NavLink>
       </nav>
     </div>
   );
